bf5ee358e58a3d9cac2a9f513eba6a700374ef36
szhangphy <szhangphy@gmail.com>
2026-02-10 02:07:53 +0800

    test

--- src_pw/unified_main.f90
+++ src_pw/unified_main.f90
@@ -3,9 +3,18 @@ program irssg
     use driver_wann, only: run_wann
     implicit none
 
+    character(len=16) :: fdbg
+    integer :: fdbg_len, fdbg_stat
+
     integer :: narg, iarg, stat, lens
     character(len=256) :: arg
     logical :: use_wann
+
+    call get_environment_variable("IRSSG_FDBG", fdbg, length=fdbg_len, status=fdbg_stat)
+    if (fdbg_stat == 0 .and. fdbg_len > 0) then
+        write(6,*) 'IRSSG Fortran main start'
+        call flush(6)
+    endif
 
     use_wann = .false.
     narg = command_argument_count()
@@ -33,4 +42,3 @@ program irssg
     end if
 
 end program irssg
-
